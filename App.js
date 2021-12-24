34f6ca532e870fba1df5a707d15ae6e3a0d28688
Пользователь <ixtifidanza@gmail.com>
2021-12-24 18:07:53 +0500

    tab is done

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Home from './screens/Home';
+
+export default function App() {
+  return <Home/>
+}
+
+
---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import HeaderTabs from '../components/HeaderTabs'
+
+export default function Home() {
+    return (
+        <SafeAreaView>
+            <HeaderTabs />
+        </SafeAreaView>
+    )
+}
